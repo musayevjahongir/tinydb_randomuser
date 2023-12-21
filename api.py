03e83de5db617c174cfe90a9715b47f19f6b9e8a
musayevjahongir <musayevjahongir5051@gmail.com>
2023-12-21 21:43:15 +0500

    random user

--- api.py
+++ api.py
@@ -1,3 +1,24 @@
 import requests
 
-base_url = "https://randomuser.me/api/"
+
+def get_users_by_gender(gender: str, n: int) -> list[dict]:
+    url = 'https://randomuser.me/api/'
+    payload = {
+        'gender': gender,
+        'results': n
+    }
+
+    resp = requests.get(url, params=payload)
+
+    users = []
+    if resp.status_code == 200:
+        for user in resp.json()['results']:
+            users.append({
+                "first_name": user['name']['first'],
+                "last_name": user['name']['last'],
+                "country": user['location']['country'],
+                "age": user['dob']['age'],
+                "nat": user['nat'],
+            })
+        return users
+    return []
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from tinydb import TinyDB
+from api import get_users_by_gender
 
-db = TinyDB('data.json')
+db = TinyDB('data.json', indent=4)
+
+male=get_users_by_gender("male", 30)
+female=get_users_by_gender("female", 30)
+
+males=db.table(name='males')
+females=db.table(name='females')
+
+
+for i in male:
+    males.insert(i)
+for i in female:
+    females.insert(i)
